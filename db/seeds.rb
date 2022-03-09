0b134d7c6293f3db39ebc081b50f74d5206eca5e
nomi-kazu <nomikazyu@gmail.com>
2022-03-09 22:58:41 +0900

    feat: 記事一覧を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create({title: 'title1', content: 'content1'})
+Article.create({title: 'title2', content: 'content2'})
