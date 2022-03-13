db91fc6c89fafc639841c0e1df58324151439d30
nomi-kazu <nomikazyu@gmail.com>
2022-03-13 16:29:56 +0900

    feat: createアクションを追加、コメント一覧を表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,21 @@ class CommentsController < ApplicationController
     article = Article.find(params[:article_id])
     @comment = article.comments.build
   end
+
+  def create
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comment_params)
+    if @comment.save
+      redirect_to article_path(article), notice: 'コメントしました'
+    else
+      flash.now[:error] = 'コメントできませんでした'
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
